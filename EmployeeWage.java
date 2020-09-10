fe81c76e740e30fa46304564b7754329cee04bdd
Swapnil Sharma <sharma.swapnil245@gmail.com>
2020-09-10 19:05:35 +0530

    Attendance added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,6 +2,7 @@ public class EmployeeWage{
 	public static void main(String [] args){
 		int isPresent=1;
 		double empCheck=Math.floor(Math.random()*10)%2;
+		System.out.println("Welcome to Employee Wage Calculator");
 		System.out.println("Empcheck"+empCheck);
 		if(empCheck==isPresent){
 			System.out.println("Employee is Present");
